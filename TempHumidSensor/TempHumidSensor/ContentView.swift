5543df947c7ca2f3c00bcc0893ff6ec3a5c0b430
STEVEN COUL <stevencoul@STEVENs-Mac-Pro.local>
2020-09-09 15:58:04 -0500

    A

---
+++ TempHumidSensor/TempHumidSensor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempHumidSensor
+//
+//  Created by STEVEN COUL on 9/9/20.
+//  Copyright © 2020 STEVEN COUL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
